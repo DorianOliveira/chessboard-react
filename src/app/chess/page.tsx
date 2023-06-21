48db0756167e366b3d2d9d190368438c64dc3f12
Dorian <dorian_oliveira@yahoo.com.br>
2023-06-21 17:16:07 -0300

    Pieces moving

--- src/app/chess/page.tsx
+++ src/app/chess/page.tsx
@@ -2,76 +2,170 @@
 
 import React, { useState, useEffect } from "react";
 import styles from "./styles.module.css";
-import { IChessPiece } from "./../../interfaces/chessboard";
+import {
+  IChessPiece,
+  ICell,
+  IChessPieceProps,
+} from "./../../interfaces/chessboard";
 
-function ChessboardCell(props) {
-  // const [piece, setPiece] = useState<IChessPiece>(null);
-  const [isMoving, setIsMoving] = useState<boolean>(false);
+function ChessboardCell(props: ICellProps) {
+  const positionStyle = props.type === "odd" ? styles.odd : styles.even;
+  const movingStyle = props.isMoving ? styles.moving : "";
+  const className = `${styles.square} ${positionStyle} ${movingStyle}`;
 
-  const positionStyle = props.isStart ? styles.start : styles.end;
-  const movingStyle = isMoving ? styles.moving : "";
+  function onPieceSelected() {
+    props.onPieceSelected(props.chessPiece);
+  }
 
-  // useEffect(() => {
-  //   if (props.piece) setPiece(state => piece);
-  // }, []);
+  function onClick() {
+    props.onCellClick(props.position, props.chessPiece);
+  }
 
   return (
-    <div
-      onMouseDown={() => {
-        setIsMoving((_) => !_);
-        props.onCellMouseDown(props.piece);
-      }}
-      className={`${styles.square} ${positionStyle} ${movingStyle}`}
-    >
-      {props.piece ? <ChessPiece /> : null}
+    <div className={className} onClick={onClick}>
+      <strong style={{ color: "#ccc" }}>{props.position}</strong>
+      {props.chessPiece ? (
+        <ChessPiece
+          onSelected={onPieceSelected}
+          chessPiece={props.chessPiece}
+        />
+      ) : null}
     </div>
   );
 }
 
-function ChessPiece() {
+function ChessPiece(props: IChessPieceProps) {
   const [piece, setPiece] = useState<IChessPiece>(null);
 
-  return <div className={styles.chesspiece} />;
+  function onChessPieceSelected() {
+    props.onSelected(props.piece);
+  }
+
+  return (
+    <div onMouseDown={onChessPieceSelected} className={styles.chesspiece} />
+  );
 }
 
-function Chessboard(props) {
+function Chessboard() {
   // const [cells, setCells] = useState<ChessboardCell[]>(null);
 
-  const [isBoardMount, setIsBoardMount] = useState(true);
   const [selectedPiece, setSelectedPiece] = useState<IChessPiece>(null);
+  const [oldPosition, setOldPosition] = useState<boolean>(false);
 
-  let isStart = false;
+  const [grid, setGrid] = useState<ICell[]>([]);
 
+  useEffect(() => mount(), []);
 
-  const items = [];
+  function onCellClick(position: number) {
+    const newGrid = grid.map((gridItem) => {
+      if (gridItem.position === position)
+        return {
+          ...gridItem,
+          chessPiece: selectedPiece,
+        };
 
-  for (let index = 0; index < 64; index++) {
-    if (index % 8 === 0) isStart = !isStart;
+      if (gridItem.position === selectedPiece.position)
+        return {
+          ...gridItem,
+          chessPiece: null,
+        };
 
-    let cellPiece: IChessPiece = null;
+      return gridItem;
+    });
 
-    
-    if ((index >= 0 && index < 16) || (index >= 48 && index <= 64))
-      cellPiece = {
-        title: `ChessPiece-${index}`,
-      } as IChessPiece;
-
-    items.push(
-      <ChessboardCell
-        onCellMouseDown={(piece: IChessPiece) => {
-          if (!selectedPiece) setSelectedPiece(piece);
-        }}
-        piece={cellPiece}
-        key={index}
-        isStart={isStart}
-      />
-    );
+    setGrid(newGrid);
   }
+
+  function onPieceSelected(piece: IChessPiece) {
+    const gridItem = grid.find((cell) => cell.position === piece.position);
+    setSelectedPiece(gridItem.chessPiece);
+
+    // if (!selectedPiece) {
+    //   setSelectedPiece(piece);
+    // } else {
+    //   setOldPosition(selectedPiece.position);
+    //   setSelectedPiece({
+    //     ...selectedPiece,
+    //     position,
+    //   });
+    // }
+  }
+
+  function mount() {
+    if (grid?.length === 0) {
+      const items = [];
+
+      let type = "odd";
+
+      for (let index = 0; index < 64; index++) {
+        if (index % 8 === 0) type = type === "odd" ? "even" : "odd";
+
+        let cellPiece: IChessPiece = null;
+
+        const isInitialPosition =
+          (index >= 0 && index < 16) || (index >= 48 && index <= 64);
+
+        if (isInitialPosition) {
+          cellPiece = {
+            position: index,
+            title: `ChessPiece-${index}`,
+          } as IChessPiece;
+        }
+
+        items.push({
+          position: index,
+          chessPiece: cellPiece,
+          type,
+        } as ICell);
+      }
+      setGrid(items);
+    }
+  }
+
+  // for (let index = 0; index < 64; index++) {
+  //   if (index % 8 === 0) type = type === 'odd' ? 'even' : 'odd'
+
+  //   let cellPiece: IChessPiece = null;
+  //   let position = selectedPiece?.position;
+
+  //   const isInitialPosition =
+  //     (index >= 0 && index < 16) ||
+  //     (index >= 48 && index <= 64 && index !== oldPosition);
+
+  //   if (isInitialPosition && !position) position = index;
+
+  //   if (position === index || isInitialPosition) {
+  //     cellPiece = {
+  //       position,
+  //       title: `ChessPiece-${position}`,
+  //     } as IChessPiece;
+  //   }
+
+  //   items.push({
+  //     position: index,
+  //     chessPiece: cellPiece,
+  //     type
+  //   } as ICell);
+  // }
+
+  console.log(grid);
 
   return (
     <div className={styles.container}>
       <h2>Chess</h2>
-      <div className={styles.chessboard}>{items}</div>
+      <div className={styles.chessboard}>
+        {grid.map((cell) => (
+          <ChessboardCell
+            onCellClick={onCellClick}
+            onPieceSelected={onPieceSelected}
+            isMoving={selectedPiece?.position === cell.position}
+            position={cell.position}
+            chessPiece={cell.chessPiece}
+            type={cell.type}
+            key={cell.position}
+          />
+        ))}
+      </div>
     </div>
   );
 }
--- src/interfaces/chessboard.ts
+++ src/interfaces/chessboard.ts
@@ -1,3 +1,25 @@
 export interface IChessPiece {
+  position: number;
   title: string;
+}
+
+export interface ICell {
+  position: number;
+  chessPiece: IChessPiece;
+  line: 'odd' | 'even';
+}
+
+export interface ICellProps {
+  position: number;
+  chessPiece: IChessPiece;
+  isStart: boolean;
+  isMoving: boolean;
+  onPieceSelected: (piece: IChessPiece) => void;
+  onCellClick: (position: number, piece: IChessPiece) => void;
+}
+
+export interface IChessPieceProps {
+  onSelected: (piece: IChessPiece) => void;
+  piece: IChessPiece;
+
 }
